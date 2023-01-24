716d2f308d898c07d9cec9821378a44263e49054
Adam Menz <mz.adamQ@gmail.com>
2023-01-24 17:04:46 -0500

    commiting before refatoring to use redux..."

--- src/utils/presetTracks.js
+++ src/utils/presetTracks.js
@@ -11,7 +11,62 @@ const tracks = [
 
   [[{on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0}],
   [{on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}],
-  [{on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0},{on: false, velocity: 1.0} , {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}]]
+  [{on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0}, {on: true, velocity: 1.0}, {on: false, velocity: 1.0},{on: false, velocity: 1.0} , {on: false, velocity: 1.0}, {on: false, velocity: 1.0}, {on: false, velocity: 1.0}]],
+
+  [
+    [
+        {on: true, velocity: 0.8},
+        {on: false, velocity: 1.0},
+        {on: true, velocity: 0.9},
+        {on: false, velocity: 1.0},
+        {on: true, velocity: 0.8},
+        {on: false, velocity: 1.0},
+        {on: true, velocity: 0.9},
+        {on: false, velocity: 1.0},
+        {on: true, velocity: 0.8},
+        {on: false, velocity: 1.0},
+        {on: true, velocity: 0.9},
+        {on: false, velocity: 1.0},
+        {on: true, velocity: 0.8},
+        {on: false, velocity: 1.0},
+        {on: true, velocity: 0.9},
+        {on: false, velocity: 1.0}
+    ],
+    [
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: true, velocity: 0.8},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: true, velocity: 0.9},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0}
+    ],
+    [ // fun fact - this one was written by chatGPT
+        {on: true, velocity: 0.8},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: true, velocity: 0.9},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: true, velocity: 0.8},
+        {on: false, velocity: 1.0},
+        {on: true, velocity: 0.9},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: false, velocity: 1.0},
+        {on: true, velocity: 0.9}]]
 
 ];
 
